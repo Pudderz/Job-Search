fda5f1494d7dc1856136fd591d657edf24af0317
Pudderz <mpudney6@gmail.com>
2020-09-22 11:25:25 +0100

    Updates

---
+++ precache-manifest.4b824c16a47b179204edf49da8c5259e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7dcbfcb7ee6f206cf001a57d05c1f5",
+    "url": "/Job-Search/index.html"
+  },
+  {
+    "revision": "afb3211a8d283677f600",
+    "url": "/Job-Search/static/css/main.02a221a3.chunk.css"
+  },
+  {
+    "revision": "d5bbc5213b9e2b399dda",
+    "url": "/Job-Search/static/js/2.c8864f69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Job-Search/static/js/2.c8864f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb3211a8d283677f600",
+    "url": "/Job-Search/static/js/main.0ea161eb.chunk.js"
+  },
+  {
+    "revision": "d52e69e93dcebbc4b323",
+    "url": "/Job-Search/static/js/runtime-main.32e64576.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/Job-Search/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/Job-Search/static/media/bg-header-mobile.6b5ff5a0.svg"
+  }
+]);
