48fd9e55ce525d5107f4f860ade3677060c9879b
Pudderz <mpudney6@gmail.com>
2020-09-29 11:46:38 +0100

    Created shouldComponentUpdate function for each job item
    This compares the old props to the new props to see whether it should update.
     Used this site as a reference to help me: https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/update/using_should_component_update.html

--- client/src/Components/jobBlock.js
+++ client/src/Components/jobBlock.js
@@ -4,8 +4,44 @@ import Link from './link'
 
 
 class JobBlock extends React.Component{
-    shouldComponentUpdate = (nextProps, nextState)=>{
 
+    //Compares the old props to the new props to see whether it should update
+    // used this site as a reference: 
+    //https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/update/using_should_component_update.html
+    
+    compare=(previousProps, newProps)=>{
+        //test if same object
+        if(previousProps === newProps){
+            return true
+        }
+        //tests if bother props objs are not null and are objects
+        if (typeof previousProps !== 'object' || previousProps === null ||
+        typeof newProps !== 'object' || newProps === null) {
+        return false;
+        }
+
+        //Creates array of keys from each props so we can test if they are the same length
+        //and has the same values in the for loop
+
+        const oldPropsKeys = Object.keys(previousProps);
+        const newPropsKeys = Object.keys(newProps);
+
+        if (oldPropsKeys.length !== newPropsKeys.length) {
+            return false;
+        }
+        
+        const newPropsHasOwnProperty = hasOwnProperty.bind(newProps);
+        for (var i = 0; i < oldPropsKeys.length; i++) {
+            if (!newPropsHasOwnProperty(oldPropsKeys[i]) || previousProps[oldPropsKeys[i]] !== newProps[oldPropsKeys[i]]) {
+            return false;
+            }
+        }
+        return true;
+    }
+
+
+    shouldComponentUpdate = (nextProps)=>{
+        return !(this.compare(this.props, nextProps))
     }
     render(){
         return(
